324f1661d1e406e0195421b1b61a25dd7f4d4bf1
Crawl <icrawltogo@gmail.com>
2018-06-15 13:07:54 +0200

    tests: make test work with new changes

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,15 @@
 process.env.NODE_ENV = 'test';
-process.env.PORT = 9901;
+process.env.PORT = 9991;
 const server = require('../src/index');
 const fetch = require('node-fetch');
 
 describe('validation', () => {
 	it('should return a valid discord webhook', async () => {
 		const body = {
-			showname: 'Test1',
-			episode_name: 'Test2',
-			episode_num: 'Test3',
-			poster_url: 'Test4',
-			plex_url: 'Test5'
+			title: 'Test1',
+			color: '3394611',
+			description: 'Test2',
+			thumbnail: { url: 'Test3' }
 		};
 
 		const res = await fetch(`http://localhost:${process.env.PORT}/api/plex/recently_added`, {
@@ -20,18 +19,7 @@ describe('validation', () => {
 		});
 		const json = await res.json();
 
-		expect(json).toEqual({
-			embeds: [
-				{
-					title: body.showname,
-					color: 0x33cc33,
-					description: `${body.episode_name} (Episode: ${body.episode_num}) has been added to Plex!\n\nWatch here: [Plex Web](${body.plex_url})`,
-					thumbnail: {
-						url: body.poster_url
-					}
-				}
-			]
-		});
+		expect(json).toEqual({ embeds: [body] });
 
 		server.server.close();
 	});
