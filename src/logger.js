b9f04b3a264f123aafde9d451058dbb0fda4eee7
Crawl <icrawltogo@gmail.com>
2018-08-19 03:11:32 +0200

    chore: use better timestamp for console

--- src/logger.js
+++ src/logger.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 const logger = winston.createLogger({
 	format: winston.format.combine(
 		winston.format.colorize({ all: true }),
-		winston.format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' }),
+		winston.format.timestamp({ format: 'YYYY/MM/DD HH:mm:ss' }),
 		winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
 	),
 	transports: [new winston.transports.Console()]
